b28906fea70b3f3febd2f5aef4c3afd07815289b
Administrator <Administrator@UMZYHEMP3VOMU7X>
2017-11-05 23:26:58 +0800

    delete duplicate dependencise

--- chinasofti-mall-app/src/main/java/com/chinasofti/app/controller/AppController.java
+++ chinasofti-mall-app/src/main/java/com/chinasofti/app/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.chinasofti.app.controller;
 
 public class AppController {
-
+	public static void main(String[] args) {
+		
+	}
 }
--- chinasofti-mall-configuration-center/src/main/java/com/chinasofti/mall/configuration/center/ConfigurationCenterApplication.java
+++ chinasofti-mall-configuration-center/src/main/java/com/chinasofti/mall/configuration/center/ConfigurationCenterApplication.java
@@ -1,5 +1,7 @@
 package com.chinasofti.mall.configuration.center;
 
 public class ConfigurationCenterApplication {
-
+	public static void main(String[] args) {
+		
+	}
 }
